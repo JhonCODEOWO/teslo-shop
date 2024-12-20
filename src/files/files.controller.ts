cad5a14e8f17e9b24d8dc21ffefc2b3029514d4d
JhonCode <jjv20618@gmail.com>
2024-12-20 13:13:58 -0600

    Now the files get a name with extension type and are saved in static/uploads

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFiler.helper';
 import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
 
 @Controller('files')
 export class FilesController {
@@ -13,11 +14,13 @@ export class FilesController {
     fileFilter: fileFilter,
     //limits: {fileSize: 1000} limites de subida
     storage: diskStorage({
-      destination: './static/uploads' //Indicar en que ruta del filesystem se guarda
+      destination: './static/uploads', //Indicar en que ruta del filesystem se guarda
+      filename:  fileNamer //Utilizar función para devolver un nombre
     })
   })) //Interceptar la llave file en la petición.
   uploadProductImage(@UploadedFile() file: Express.Multer.File){ //Asignar tipado del archivo que esperamos recibir
 
+    //El file en este apartado ya contiene información sobre el archivo con los datos a donde se va a mover y de donde viene.
     if(!file) throw new BadRequestException('Make sure to upload a image');
 
     return file.originalname;
