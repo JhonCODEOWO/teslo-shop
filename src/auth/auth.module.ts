11d9a6159e85000f7d2d9ef3056b1f4405abaea5
Jonathan Juárez Valera <jjv20618@gmail.com>
2024-12-30 10:27:46 -0600

    Modulo asíncrono configurado para JWT.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,37 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/users.entity';
 import { Type } from 'class-transformer';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+
+    PassportModule.register({defaultStrategy: 'jwt'}),
+
+    // JwtModule.register({
+    //   secret: '123456',
+    //   signOptions: {
+    //     expiresIn: '2h'
+    //   }
+    // })
+
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ], //Importar módulos
+      inject: [ConfigService], //Inyectar servicios u otros elementos pertenecientes a los módulos importados.
+      useFactory: ( configService: ConfigService ) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h'
+          }
+        }
+      }
+    })
   ],
   exports: [TypeOrmModule]
 })
