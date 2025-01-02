f545d07a4e6d5dbae2715a6826a2f67ee63aed05
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-01-02 12:14:42 -0600

    Validate function finished and some settings added to auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,11 +7,14 @@ import { Type } from 'class-transformer';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.stragety';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+    ConfigModule,
+
     TypeOrmModule.forFeature([User]),
 
     PassportModule.register({defaultStrategy: 'jwt'}),
@@ -36,6 +39,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }
     })
   ],
-  exports: [TypeOrmModule]
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule]
 })
 export class AuthModule {}
--- src/auth/strategies/jwt.stragety.ts
+++ src/auth/strategies/jwt.stragety.ts
@@ -1,12 +1,36 @@
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { ExtractJwt, Strategy } from "passport-jwt";
 import { User } from "../entities/users.entity";
 import { JwtPayload } from "../interfaces/jwt-payload.interface";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { ConfigService } from "@nestjs/config";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy){
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+
+        configService: ConfigService
+    ){
+        //Aplicar valores a el constructor principal
+        super({
+            secretOrKey: configService.get('JWT_SECRET'),
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), //Envíar token como bearertoken
+        });
+    }
+
+    //Se va a ejecutar siempre y cuando el token siga activo y el token haga match con el payload
     async validate(payload: JwtPayload): Promise<User>{
         const {email} = payload;
-        
-        return ;
+        const user = await this.userRepository.findOneBy({email});
+
+        if(!user) throw new UnauthorizedException('Token no valid');
+
+        if(!user.isActive) throw new UnauthorizedException('User is inactive, talk with admin');
+
+        return user; //Este dato retornado se va a añadir al request general
     }
 }
