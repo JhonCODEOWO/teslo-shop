1ab61efc651d231f4f136d4cc2209fb47ed8f165
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-01-13 12:46:43 -0600

    Just a little import to use Auth() decorator in another modules, check the changes on seed.module.ts

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { validRoles } from 'src/auth/interfaces/valid-roles';
 
 @Controller('seed')
 export class SeedController {
