d3260825f81ad9c9e191ec0a0c1542c03d1bd25c
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-01-23 11:46:33 -0600

    Now the server identifies if a handshake token is valid and if the user exists with isActive in true to be able to make a connection to the ws.

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -3,22 +3,32 @@ import { MessagesWsService } from './messages-ws.service';
 import { Socket } from 'socket.io';
 import { Server } from 'http';
 import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
 
 @WebSocketGateway({cors: true})
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   
   @WebSocketServer() wss: Server;
-  constructor(private readonly messagesWsService: MessagesWsService) 
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService
+  ) 
   {}
 
-  handleConnection(client: Socket, ...args: any[]) {
+  async handleConnection(client: Socket, ...args: any[]) {
 
     const token = client.handshake.headers.token as string;
-    console.log(token);
-
-    this.messagesWsService.registerClient(client);
-
-    console.log(`Clients connected: ${this.messagesWsService.getConnectedClients()}`);
+    let payload: JwtPayload;
+    
+    //Obtener el payload del token y si no es válida negar la conexión
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
 
     //Emitir a todos los usuarios un mensje
     //Sintaxis: Evento, Payload a enviar (Normalmente es un json)
@@ -27,8 +37,6 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
-
-    console.log(`Clients connected: ${this.messagesWsService.getConnectedClients()}`);
 
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients()); //Envíar los id de los clientes conectados
   }
@@ -50,9 +58,11 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
     //   message: payload.message || 'No message'
     // })
 
+    console.log(payload);
+
     //Emitir un mensaje a todos los clientes conectados
     this.wss.emit('message-from-server', {
-      fullName: 'Yo mismo xd',
+      fullName: this.messagesWsService.getUserFullNameBySocket(client.id),
       message: payload.message || 'No message'
     })
   }
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,21 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Socket } from 'socket.io';
+import { User } from 'src/auth/entities/users.entity';
+import { Repository } from 'typeorm';
 
 interface connectedClients {
     //Define un objeto en donde la clave es string y los valores son de tipo Socket es decir una clase de Socket
-    [id: string]: Socket
+    [id: string]: {
+        socket: Socket,
+        user: User
+    }
 }
 
 @Injectable()
 export class MessagesWsService {
-
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>
+    ){}
     //Los clientes se mantienen en memoria porque son datos volatiles, cada id cambia constantemente dependiendo de quien se conecta
     private connectedClients: connectedClients = { }
 
     //Método a ejecutar cuando un cliente se conecta
-    registerClient(client: Socket){
-        //Añadir un nuevo elemento en donde id es string y su valor es un objeto Socket
-        this.connectedClients[client.id] = client;
+    async registerClient(client: Socket, userId: string){
+        //Intentar obtener el usurio
+        const user = await this.userRepository.findOneBy({id: userId})
+
+        //Validar posibles problemas con el usuario y mandar un error
+        if(!user) throw new Error('User not found');
+        if(!user.isActive) throw new Error('User not active');
+
+        //Añadir un nuevo elemento en donde id es string y su valor es un objeto con dos claves Socket y User
+        this.connectedClients[client.id] = {
+            socket: client,
+            user: user
+        };
     }
 
     //Método a ejecutar cuando un cliente se desctonecta
@@ -28,5 +47,10 @@ export class MessagesWsService {
     getConnectedClients(): string[]{
         //Retorna un arreglo de strings con las claves del objeto que recibe como parámetro
         return Object.keys(this.connectedClients);
+    }
+
+    //Retorna el nombre ligado a ese socket id del usuario
+    getUserFullNameBySocket(socketId: string): string{
+        return this.connectedClients[socketId].user.fullname;
     }
 }
